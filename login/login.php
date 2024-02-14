29b3a05e1df93332fddfb1457133d0b464100a59
SebaCM <seba1carrillo@gmail.com>
2024-02-14 07:02:57 -0600

    Se cambio la version de jquery

--- login/login.php
+++ login/login.php
@@ -18,10 +18,12 @@ if(isset($_SESSION["user_name"])){
   <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="../node_modules/sweetalert2/dist/sweetalert2.min.css">
-    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <!--<script src="../node_modules/jquery/dist/jquery.min.js"></script>-->
     <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script src="../node_modules/sweetalert2/dist/sweetalert2.min.js"></script>
+    
  
 </head>
 <body>
