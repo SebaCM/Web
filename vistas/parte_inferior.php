5c430843d1038a4cabeea745e663b4403f8390ba
SebaCM <seba1carrillo@gmail.com>
2024-02-22 19:21:56 -0600

    Se adapto la conexion de las vistas

--- vistas/parte_inferior.php
+++ vistas/parte_inferior.php
@@ -37,7 +37,7 @@
            <div class="modal-body">Presione "Logout" si quieres terminar la sesion.</div>
            <div class="modal-footer">
                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-               <a class="btn btn-primary" href="../login/logout.php">Logout</a>
+               <a class="btn btn-primary" href="login/logout.php">Logout</a>
            </div>
        </div>
    </div>
