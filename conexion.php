bd7bb5c34c4cfe307e20b1f1950acc33ed3d44d7
SebaCM <seba1carrillo@gmail.com>
2024-02-22 18:04:05 -0600

    Se cambiaron las credenciales de conexion

--- conexion.php
+++ conexion.php
@@ -1,17 +1,16 @@
 <?php
 class Conexion{
     public static function Conectar(){
-        define('servidor','162.240.38.95');
-        define('nombre_bd','wwraco_mqtt_data');
-        define('usuario','wwraco_mqtt_root');
+        define('servidor','localhost');
+        define('nombre_bd','mqtt_data');
+        define('usuario','root');
         define('password','Iwf5Lp1_aYFV');
-        define('puerto','443');
         $opciones=array(
             PDO::ATTR_EMULATE_PREPARES=>FALSE,
             PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');
         try{
-            $conexion=new PDO("mysql:host=".servidor.";port=".puerto.";dbname=".nombre_bd,usuario,password,
+            $conexion=new PDO("mysql:host=".servidor.";dbname=".nombre_bd,usuario,password,
             $opciones);
             return $conexion;
         }catch (Exception $e){
